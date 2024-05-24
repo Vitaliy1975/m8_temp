a01b005360d77819063530e9b5889ac285393dc9
Vitaliy1975 <assa175@i.ua>
2024-05-24 03:34:54 +0300

    modified:   __pycache__/models.cpython-311.pyc 	modified:   main.py 	modified:   models.py 	modified:   seed.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-from models import Authors,Quotes
+from models import Authors, Quotes
 import connect
 
 while True:
-    a=input("To get quotes by author enter command like 'name:author'\nTo get quotes by tag enter commant like 'tag:tag1,tag2...'\n'exit' for exit. ")
-    if a=="exit":
+    a = input(
+        "To get quotes by author enter command like 'name:author'\nTo get quotes by tag enter command like 'tag:tag1,"
+        "tag2...'\n'exit' for exit. ")
+    if a == "exit":
         break
     else:
-        k=a.split(":")[0]
-        v=a.split(":")[1]
-        v_v=v.split(",")
-    if k=="name":
-        authors=Authors.objects(fullname=v)
-        quotes=Quotes.objects(author=v)
-        for a in authors:
-            print(a.to_mongo().to_dict())
+        k = a.split(":")[0]
+        v = a.split(":")[1]
+        v_v = v.split(",")
+    if k == "name":
+        auth=Authors.objects(fullname=v)
+        quotes = Quotes.objects(author=auth)
         for q in quotes:
-            print(q.to_mongo().to_dict())
-            # if a.id==q.id:
-    if k=="tag":
-        quotes=Quotes.objects(tags=v)
+            print(q.to_mongo())
+        for a in auth:
+            print(a.to_mongo())
+    if k == "tag":
+        quotes = Quotes.objects(tags=v)
         for q in quotes:
-            print(q.tags,q.quote)
-    if k=="tags":
-        quotes=Quotes.objects(tags__in=v_v)
+            print(q.tags, q.quote)
+    if k == "tags":
+        quotes = Quotes.objects(tags__in=v_v)
         for q in quotes:
-            print(q.tags,q.quote)
+            print(q.tags, q.quote)
--- seed.py
+++ seed.py
@@ -2,14 +2,18 @@ from models import Authors, Quotes
 import connect
 import json
 
-with open("authors.json","rb") as f:
-    data_authors=json.load(f)
+with open("authors.json", "rb") as f:
+    data_authors = json.load(f)
 for i in data_authors:
     authors = Authors.from_json(json.dumps(i))
     authors.save()
 
-with open("quotes.json","rb") as f:
-    data_quotes=json.load(f)
+with open("quotes.json", "rb") as f:
+    data_quotes = json.load(f)
 for i in data_quotes:
-    quotes=Quotes.from_json(json.dumps(i))
+    for j in i.items():
+        if j[0]=="author":
+            i["author"]=Authors.objects.get(fullname=j[1])
+for i in data_quotes:
+    quotes = Quotes(**i)
     quotes.save()
